17916df3959912d7af9662057142fdd5b05e3cb8
gardenia-inkart <135148181+gardenia-inkart@users.noreply.github.com>
2023-06-01 01:30:51 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('#mobile-menu').click(function(){
+        $('#menu-modal').slideDown();
+    });
+    $('#close-modal').click(function(){
+        $('#menu-modal').slideUp();
+    });
+});
